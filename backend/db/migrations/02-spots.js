a295e59b57bfc10bb98a60df79ce38e7efca0b2f
koderny <haroldpedraza@gmail.com>
2025-02-03 19:10:09 -0500

    created tables

---
+++ backend/db/migrations/02-spots.js
@@ -0,0 +1,65 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Spots', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      address: {
+        type: Sequelize.STRING(700),
+        allowNull: false,
+        unique: true
+      },
+      city: {
+        type: Sequelize.STRING(50),
+        allowNull: false
+      },
+      state: {
+        type: Sequelize.STRING(50),
+        allowNull: false
+      },
+      country: {
+        type: Sequelize.STRING(50),
+        allowNull: false
+      },
+      lat: {
+        type: Sequelize.DECIMAL(100.00),
+        allowNull: true
+      },
+      lng: {
+        type: Sequelize.DECIMAL(100.00),
+        allowNull: true
+      },
+      name: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+        unique: true
+      },
+      description: {
+        type: Sequelize.STRING(1000),
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.DECIMAL(1000.00),
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Spots');
+  }
+};
