f1a21810d4145180aa129a3256bd51f89a134502
tallha07 <tallhamuddassar@yahoo.com>
2025-02-06 19:56:08 -0600

    changed the R to r and fixed dates

--- backend/db/seeders/04-booking.js
+++ backend/db/seeders/04-booking.js
@@ -12,28 +12,28 @@ module.exports = {
       {
         spotId: 1,
         userId: 1,
-        startDate: Date ('2025-01-10'),
-        endDate: Date ('2025-01-15')
+        startDate: '2025-01-10',
+        endDate: '2025-01-15'
       }, {
         spotId: 2,
         userId: 2,
-        startDate: Date ('2024-02-11'),
-        endDate: Date ('2024-02-15')
+        startDate: '2024-02-11',
+        endDate: '2024-02-15'
       }, {
         spotId: 3,
         userId: 3,
-        startDate: Date ('2023-03-09'),
-        endDate: Date ('2023-03-15')
+        startDate: '2023-03-09',
+        endDate: '2023-03-15'
       }, {
         spotId: 4,
         userId: 4,
-        startDate: Date ('2023-04-20'),
-        endDate: Date ('2023-04-25')
+        startDate: '2023-04-20',
+        endDate: '2023-04-25'
       }, {
         spotId: 5,
         userId: 5,
-        startDate: Date ('2022-05-30'),
-        endDate: Date ('2022-06-04')
+        startDate: '2022-05-30',
+        endDate: '2022-06-04'
       },
 
     ], { validate: true });
