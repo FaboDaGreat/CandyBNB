64932e6fe0b80cb2b41064ca7ce30b0975604efa
Chocolatebear33 <fabian.richmond92@gmail.com>
2025-02-09 21:11:14 -0500

    Changed all spot relationships back to ownerId from userId

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       User.hasMany(models.Spot, {
-        foreignKey: "userId",
+        foreignKey: "ownerId",
         onDelete: "CASCADE",
         hooks: true
       });
